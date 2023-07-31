2e63f371f8b92fa45010f2310a4d47e10a3540c6
Samanjoy Das <samanjoydas964@gmail.com>
2023-07-31 23:55:55 +0530

    Update Girl Voice Python.py
    Added comments

--- Girl Voice Python.py
+++ Girl Voice Python.py
@@ -3,9 +3,9 @@ import pyttsx3
 friend = pyttsx3.init()
 voices = friend.getProperty('voices')
 friend.setProperty('voice', voices[1].id) #voice[0] => male
-friend.setProperty('rate', 150)
-friend.say("Samanjoy")
+friend.setProperty('rate', 150) #150 is delay time
+friend.say("MESSAGE")
 friend.setProperty('rate', 100)
-friend.say("I Love You.")
+friend.say("MESSAGE")
 friend.runAndWait()
 friend.stop()
